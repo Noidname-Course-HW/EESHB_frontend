933b2bd666c6c082d19868ab22d08206ec291181
noidname <tim983649@gmail.com>
2020-06-29 20:52:44 +0800

    Private Route

---
+++ src/Manage.js
@@ -0,0 +1,17 @@
+import React,{Component} from 'react';
+ class Manage extends Component {
+     constructor(props){
+         super(props)
+         this.state={
+
+         }
+     }
+
+     render(){
+         return(<div>
+                Manage
+                </div>)
+     }
+}
+
+export default Manage;
